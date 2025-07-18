38b41eb93629e88a0cb0dae22d649a07b0160d41
Jaswant Yadav <jaswantyadav742@gmail.com>
2025-07-18 19:56:25 +0530

    first commit

---
+++ db/config.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGO_URL;
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.error("MongoDB connection error:", err));
